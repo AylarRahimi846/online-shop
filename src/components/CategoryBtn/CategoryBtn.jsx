a061a038a2ed577642db2bf843986826dd93702e
AylarRahimi846 <rahimiaylar846@gmail.com>
2024-04-16 13:31:46 +0430

    Activate the filter button

---
+++ src/components/CategoryBtn/CategoryBtn.jsx
@@ -0,0 +1,30 @@
+import { useContext, useState } from 'react';
+import useFetch from "../../hooks/useFetch";
+import Products from "../context/products/Products";
+import CSS from "./categoryBtn.module.css"
+
+
+
+
+function CategoryBtn({ category, currentCat, setCurrentCat }) {
+    let data;
+    if (category == "All") {
+        data = useFetch('https://fakestoreapi.com/products')
+    }
+
+    else {
+        data = useFetch(`https://fakestoreapi.com/products/category/${category}`)
+    }
+    const { setFilteredProducts } = useContext(Products)
+
+    function filter() {
+        setFilteredProducts(data)
+        setCurrentCat(category)
+    }
+
+    return (
+        <p className={(currentCat == category) ? `${CSS.activeBtn} ${CSS.filterBtn}` : `${CSS.filterBtn}`} onClick={filter}>{category}</p>
+    )
+}
+
+export default CategoryBtn;
--- src/components/FilterBar/FilterBar.jsx
+++ src/components/FilterBar/FilterBar.jsx
@@ -1,18 +1,23 @@
+import { useState } from 'react'
 import useFetch from '../../hooks/useFetch'
 import CSS from './filterBar.module.css'
+import CategoryBtn from '../CategoryBtn/CategoryBtn'
 
 function FilterBar(){
 
     const categories = useFetch('https://fakestoreapi.com/products/categories')
+    const [currentCat , setCurrentCat] = useState("All")
     return(
         <>
            <div className={CSS.filterBar}>
+           <CategoryBtn category={"All"} currentCat={currentCat} setCurrentCat={setCurrentCat}/>
             {categories.map(category =>{
-                return <p key={category} className={CSS.filterBtn}>{category}</p>
+                return  <CategoryBtn key={category} category={category} currentCat={currentCat} setCurrentCat={setCurrentCat}/>
             })}
         </div>
         </>
     )
+
 }
 
 export default FilterBar
